fbf1f99793043bd3491630001eeaf397c74b2e5c
mxteuss <ovagabundant@gmail.com>
2025-08-20 20:11:55 -0300

    upando arquivos

---
+++ CadastroNinja/src/main/java/dev/java/CadastroNinja/CadastroNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjaApplication.class, args);
+	}
+
+}
